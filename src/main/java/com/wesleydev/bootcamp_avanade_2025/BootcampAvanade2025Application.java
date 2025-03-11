791e4fcebed2646453dd3c1f7317c2eebeb7ce7d
WESLEY GABRIEL DA SILVA SOUZA <wesleygabrielx@gmail.com>
2025-03-11 20:03:29 -0300

    Initial commit

---
+++ src/main/java/com/wesleydev/bootcamp_avanade_2025/BootcampAvanade2025Application.java
@@ -0,0 +1,13 @@
+package com.wesleydev.bootcamp_avanade_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampAvanade2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampAvanade2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleydev/bootcamp_avanade_2025/BootcampAvanade2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleydev.bootcamp_avanade_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampAvanade2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
